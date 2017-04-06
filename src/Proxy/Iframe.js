ae9e75d97dba87575fb9a634eace924911733026
Viktor Vincze <viktor.vincze@doclerholding.com>
2017-04-06 11:28:38 +0200

    Add Iframe proxy.

--- src/Proxy/Iframe.js
+++ src/Proxy/Iframe.js
@@ -1,19 +1,19 @@
 export default class IframeProxy {
 
 	constructor(Sender, Receiver) {
-		this.Sender = Sender;
-		this.Receiver = Receiver;
+		this.Sender = Sender
+		this.Receiver = Receiver
 	}
 
 	postMessage(message) {
-		this.Receiver.postMessage(message, '*');
+		this.Receiver.postMessage(message, '*')
 	}
 
 	addEventListener(handler) {
-		this.Sender.addEventListener('message', handler);
+		this.Sender.addEventListener('message', handler)
 	}
 
 	removeEventListener(handler) {
-		this.Sender.removeEventListener('message', handler);
+		this.Sender.removeEventListener('message', handler)
 	}
 }
